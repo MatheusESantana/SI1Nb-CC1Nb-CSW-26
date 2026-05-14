e0eac5c3bbbecafbb8a670a38f283a352797e224
MatheusESantana <matheuseleuteriosant@hotmail.com>
2026-05-13 22:33:11 -0300

    Add files via upload

---
+++ AULA 14/javascript.js
@@ -0,0 +1,22 @@
+var x = 10;
+
+if (x > 5) {
+    console.log("x é maior que 5");
+} else if (x === 5) {
+   
+    console.log("x é igual a 5");
+} else {
+    
+    console.log("x é menor que 5");
+}
+
+
+for (var i=0; i<10; i++) {
+    if (i % 2 ===0) {
+        console.log(i);
+    }
+}
+
+
+
+
